762b54c5e0a690404a18b368fb4eabc8085a4527
JuanGaona-15 <zs24013373@estudiantes.uv.mx>
2026-06-06 12:07:34 -0600

    plataformas

--- js/plataformasJS.js
+++ js/plataformasJS.js
@@ -1,6 +1,7 @@
 //No uso el metodo de cargar antes, debe cargar de inmediato
 let tablaPlataformas = document.getElementsByClassName("plataformas");
 let tablaTitulo = document.getElementsByClassName("titulo-plataforma");
+let tablaLogo = document.getElementsByClassName("imagen-plataforma");
 let tablaInformacion = document.getElementsByClassName("info-plataforma");
 
 // fetch (conseguir) el xml
@@ -19,11 +20,13 @@ fetch("xml/plataformas.xml")
         const xmlDoc = parser.parseFromString(dataText, "text/xml");           
         const listaPlataformas = xmlDoc.getElementsByTagName("plataforma"); // Lista de plataformas
         // Recorrer plataformas
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 6 ; i++) {
             const plataforma = listaPlataformas[i];
             const titulo = plataforma.getElementsByTagName("titulo")[0].textContent;
             const informacion = plataforma.getElementsByTagName("informacion")[0].textContent;
+            const logo = plataforma.getElementsByTagName("logo")[0].textContent;
             tablaTitulo[i].textContent = titulo;
+            tablaLogo[i].innerHTML  = `<img class="logos" width = "200px" height = "200px" src="${logo}" alt="logo">`;
             tablaInformacion[i].innerHTML = `<p>${informacion}</p>`;
         }
     })
